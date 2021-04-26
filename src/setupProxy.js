6dec0363e1a6cce5583940ecf8373bee14602d95
Brian Hsu <brianhsu305@gmail.com>
2021-04-26 16:21:53 -0700

    setup proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/search',
+    createProxyMiddleware({
+      target: 'https://api.edamam.com',
+      changeOrigin: true,
+    })
+  );
+};
