d789640e2add08bfd4ec8dbcd5b65b765fd585c7
Brian Hsu <brianhsu305@gmail.com>
2021-04-26 18:04:46 -0700

    built title link

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <h1 className="title">Recipe Dictionary</h1>
+            <a href="/"><h1 className="title">Recipe Dictionary</h1></a>
         </div>
     )
 }
