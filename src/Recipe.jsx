81346ee5fe86b0a0e549c84474ad9b7e307ba933
Brian Hsu <brianhsu305@gmail.com>
2024-05-22 15:02:46 -0700

    added relavate route to recipe details

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -4,16 +4,16 @@ const Recipe = (props) => {
 	const myEncodedUri = encodeURIComponent(props.dish.uri);
 
 	return (
-		<div className='bg-white m-4 flex gap-x-2 items-center rounded md:flex-col'>
+		<div className='bg-white my-4 flex gap-x-2 items-center rounded md:flex-col'>
 			<img src={props.dish.image} alt='dish' className='rounded w-3/12' />
 			<div className='text-black text-left flex-1 md:flex-auto'>
 				<p className='font-bold'>{props.dish.label}</p>
 				<p className=''>For {props.dish.yield} servings</p>
-				<p>{props.dish.calories.toFixed(0)} cal</p>
+				<p><span className='font-bold'>{(props.dish.calories / props.dish.yield).toFixed(0)}</span> cal per serving</p>
 			</div>
 
 			<Link
-				to={`/recipe/${myEncodedUri}`} className='mr-2 p-2 bg-black rounded'
+				to={`/recipe/${myEncodedUri}`} className='mr-2 p-2 bg-emerald-300 rounded'
 			>
 				<button className=''>More</button>
 			</Link>
--- src/RecipeDetails.jsx
+++ src/RecipeDetails.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 
-const RecipeDetails = () => {
+const RecipeDetails = (props) => {
 	const APP_ID = 'c2f2cb27';
 	const APP_KEY = import.meta.env.VITE_KEY;
 
 	const [result, setResult] = useState({});
 
 	const { id } = useParams();
+	const navigate = useNavigate();
 	console.log(`recipeId: ${id}`);
 
 	useEffect(() => {
@@ -27,41 +28,41 @@ const RecipeDetails = () => {
 				<div>
 					<section className='' id='main'>
 						<div className='mb-5'>
-							<Link to='/'>
-								<button className='rounded bg-red-900 p-2'>Go Back</button>
-							</Link>
+							{/* <Link to='/'> */}
+								<button className='rounded bg-red-400 p-2' onClick={() => navigate(-1)}>Back</button>
+							{/* </Link> */}
 							<h2 className='text-4xl font-bold mt-5'>{result.label}</h2>
 						</div>
 
 						<div className='border-100'>
 							<img src={result.image} alt='dish' />
 							<div className=''>
-								<h5 className='font-bold text-2xl text-lime-500 my-3'>Health Labels</h5>
+								<h5 className='font-bold text-2xl my-3'>Health Labels</h5>
 								<p className='flex flex-wrap'>
 									{result.healthLabels.map((item, i) => (
-										<span key={i} className='rounded bg-red-900 m-2 p-2'>{item}</span>
+										<span key={i} className='rounded bg-emerald-300 m-2 p-2'>{item}</span>
 									))}
 								</p>
 							</div>
 						</div>
 					</section>
 					<section className='ingredients'>
-						<h5 className='font-bold text-2xl text-lime-500 my-3'>Ingredients for {result.yield} serving&#40;s&#41;</h5>
+						<h5 className='font-bold text-2xl my-3'>Ingredients for {result.yield} serving&#40;s&#41;</h5>
 						{result.ingredientLines.map((item, i) => (
 							<p key={i}>{item}</p>
 						))}
 					</section>
 					<section className='nutrients'>
-						<h5 className='font-bold text-2xl text-lime-500 my-3'>Nutrients &#40;per serving&#41;</h5>
-						<p className='rounded bg-red-900 m-2 p-2'>{(result.calories / result.yield).toFixed(0)} calories</p>
-						<p className='flex flex-wrap'>
+						<h5 className='font-bold text-2xl my-3'>Nutrients &#40;per serving&#41;</h5>
+						<p className='font-bold'>{(result.calories / result.yield).toFixed(0)} calories</p>
+						<div className='flex flex-wrap'>
 							{Object.values(result.totalNutrients).map((item, i) => (
-								<span key={i} className='rounded bg-red-900 m-2 p-2'>
+								<span key={i} className='rounded bg-emerald-300 m-2 p-2'>
 									{item.label} {(item.quantity / result.yield).toFixed(0)}
 									{item.unit}
 								</span>
 							))}
-						</p>
+						</div>
 					</section>
 				</div>
 			)}
